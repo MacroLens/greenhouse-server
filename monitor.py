099e274f26d6af7243229c8469ce790ae2e2a722
Dylan Le <led2@rpi.edu>
2023-12-31 21:30:53 -0800

    monitor: add calibration code

--- monitor.py
+++ monitor.py
@@ -3,6 +3,7 @@ Saves data to sqlite db.
 """
 
 from sense_hat import SenseHat
+import numpy as np
 import numbers_image
 # import inflect
 
@@ -23,9 +24,24 @@ except sqlite3.OperationalError:
     print("Database already created")
 con.commit()
 
+def get_poly_cal(filename: str, max_degree: int=2) -> np.ndarray :
+    """
+    Calculates the least squares regression polynomial
+    curve for the CSV `filename` with N samples.
+    With maximum degree max_degree or N-1 whichever is smaller.
+    """
+    # Open calibration file
+    x,y = np.loadtxt(filename, delimiter=',', unpack=True)
+    degree = min(max_degree, len(y)-1)
+
+    # Calculate least squares regression
+    return np.polynomial.polynomial.polyfit(x,y, degree)
+
 
 s = sched.scheduler(time.time, time.sleep)
 sense = SenseHat()
+polynomial = get_poly_cal("cal.csv")
+
 # inf = inflect.engine()
 
 def rounder(t):
@@ -43,11 +59,19 @@ def c2f(c):
 def save_data(sc):
     timestamp = rounder(datetime.datetime.now()).timestamp()
 
-    temp = round(sense.get_temperature(), 1)
+    # Read 1000 temps over 5 seconds to get the avg reading
+    interval = 5 / 1000 # inteval between reads
+    temps = []
+    for _ in range(1000):
+        temps.append(sense.get_temperature())
+        time.sleep(interval)
+    temp = sum(temps)/len(temps)
+
+    calibrated_temp = round(np.polynomial.polynomial.polyval(temp, polynomial), 1)
     humidity = round(sense.get_humidity(), 1)
     pressure = int(sense.get_pressure())
 
-    stmt = f'''INSERT INTO greenhouse VALUES ({timestamp}, {temp}, {humidity}, {pressure} );'''
+    stmt = f'''INSERT INTO greenhouse VALUES ({timestamp}, {calibrated_temp}, {humidity}, {pressure} );'''
 
     dt = datetime.datetime.fromtimestamp(timestamp)
     print(f'{dt} : {temp}c, {humidity}%, {pressure}mbar')
